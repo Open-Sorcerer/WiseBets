472b0046bc74872401dc298eb4aaaa75a993ae21
vrajdesai78 <vrajdesai78@gmail.com>
2024-06-03 01:14:37 +0530

    changed title

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight">
-              Give opinions & stake through Frames
+              Give opinions & trade through Frames
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl">
               All you need to do is choose your side on live campaigns, place your bets, and wait
